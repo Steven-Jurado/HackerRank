b0e28201fa1daa3b594682c1a2b129df24d330c5
Steven Jurado M <stevejurado797@gmail.com>
2021-10-12 15:47:29 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HackerRank
+{
+    // this a test of HackerRank
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BriBirthdayCakeCandles();
+        }
+
+        
+        public static void  BriBirthdayCakeCandles()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] candles = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int maxHeight = 0;
+            int maxHeightCandles = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (candles[i] > maxHeight)
+                {
+                    maxHeight = candles[i];
+                    maxHeightCandles = 1;
+                }
+                else if (candles[i] == maxHeight)
+                {
+                    maxHeightCandles++;
+                }
+            }
+
+            Console.WriteLine(maxHeightCandles);
+        }
+    }
+}

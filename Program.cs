6b264d68bc93c535ddf9a1970d4a78eecfe006c0
Steven Jurado M <stevejurado797@gmail.com>
2021-10-15 11:30:58 -0500

    se agrego challenge de TimeConversion

--- Program.cs
+++ Program.cs
@@ -1,52 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace HackerRank
+﻿namespace HackerRank
 {
+    using System;
+    using System.Collections.Generic;
+
     // this a test of HackerRank
     class Program
     {
         static void Main(string[] args)
         {
-           // BriBirthdayCakeCandles();
             Console.WriteLine( TimeConversion( Console.ReadLine() ) );
         }
 
         
-        static void  BriBirthdayCakeCandles()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] candles = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), int.Parse);
-
-            int maxHeight = 0;
-            int maxHeightCandles = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (candles[i] > maxHeight)
-                {
-                    maxHeight = candles[i];
-                    maxHeightCandles = 1;
-                }
-                else if (candles[i] == maxHeight)
-                {
-                    maxHeightCandles++;
-                }
-            }
-
-            Console.WriteLine(maxHeightCandles);
-        }
-
-        static string TimeConversion( string time)
+        static string TimeConversion( string time )
         {
             
             if (time != null)
             {
+                string hourString = time.Split(':')[0] ;
 
                 if (time.Contains("PM"))
                 {
-
-                    var hourString = time.Split(":")[0];
                     var hourInteger = int.Parse(hourString);
 
                     Dictionary<int, string> hours = new Dictionary<int, string>();
@@ -62,24 +36,36 @@ namespace HackerRank
                     hours.Add(9,"21");
                     hours.Add(10,"22");
                     hours.Add(11,"23");
-                    hours.Add(12,"00");
+                    hours.Add(12,"12");
                     
                     foreach (var hour in hours)
                     {
 
                         if (hourInteger == hour.Key)
-                        {
-                                                        
+                        {     
                             return hour.Value+time.Substring(2,time.Length-4);
-
                         }
 
+                    }
+                }
+                else
+                {
+                    if (time.Contains("AM"))
+                    {
+                        int hourInteger = int.Parse(hourString);
+
+                        if (hourInteger == 12)
+                        {
+                            return "00"+time.Substring(2,time.Length-4);
+                        }
+
+                        return time.Substring(0, time.Length-2);
                     }
                 }
 
             }
 
-            return null;
+            return "Ingrese un valor porfavor.";
 
         }
     }

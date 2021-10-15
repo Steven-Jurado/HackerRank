2ad4df5a034c1720712c55c0b416fe9d6c10b461
Steven Jurado M <stevejurado797@gmail.com>
2021-10-15 11:33:21 -0500

    se agrego challenge de TimeConversion

--- Program.cs
+++ Program.cs
@@ -8,10 +8,34 @@
     {
         static void Main(string[] args)
         {
+            BriBirthdayCakeCandles();
             Console.WriteLine( TimeConversion( Console.ReadLine() ) );
         }
 
-        
+        static void  BriBirthdayCakeCandles()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] candles = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), int.Parse);
+
+            int maxHeight = 0;
+            int maxHeightCandles = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (candles[i] > maxHeight)
+                {
+                    maxHeight = candles[i];
+                    maxHeightCandles = 1;
+                }
+                else if (candles[i] == maxHeight)
+                {
+                    maxHeightCandles++;
+                }
+            }
+
+            Console.WriteLine(maxHeightCandles);
+        }
+
         static string TimeConversion( string time )
         {
             
